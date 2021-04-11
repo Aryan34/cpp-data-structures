3b33ac6f0adf504c97e3ff884bbfd8332699908b
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2021-04-10 21:44:49 -0400

    Implement more vector class methods

--- vector.cpp
+++ vector.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 
 template <typename T>
@@ -6,25 +7,43 @@ private:
     T *m_arr;
     unsigned int m_size;
     unsigned int m_capacity;
+    void addCapacity();
 public:
-    explicit Vector(int s=10);
+    explicit Vector(T fillValue, int s=10);
     ~Vector();
     Vector(const Vector &vector);
 
     unsigned int size();
     T get(int index);
     void set(int index, T value);
+    void fill(T value);
     void add(T value);
     T pop();
+    void insert(int index, T value);
+    void remove(int index);
     void print();
 };
 
 template <typename T>
-Vector<T>::Vector(int s) {
+void Vector<T>::addCapacity() {
+    assert(m_size == m_capacity);
+    unsigned int tmp_capacity = m_capacity % 2 == 1 ? static_cast<unsigned int>(m_capacity * 1.5 + 1) : static_cast<unsigned int>(m_capacity * 1.5);
+    T *tmp_arr = new T[tmp_capacity];
+    for (int i = 0; i < m_capacity; i++) {
+        tmp_arr[i] = m_arr[i];
+    }
+    delete[] m_arr;
+    m_arr = tmp_arr;
+    m_capacity = tmp_capacity;
+}
+
+template <typename T>
+Vector<T>::Vector(T fillValue, int s) {
     if (s > 0) {
         m_size = s;
         m_capacity = s;
         m_arr = new T[m_capacity];
+        fill(fillValue);
         std::cout << "Constructor called." << std::endl;
     }
     else throw ("Size cannot be less than one.");
@@ -68,16 +87,16 @@ void Vector<T>::set(int index, T value) {
 }
 
 template <typename T>
+void Vector<T>::fill(T value) {
+    for (int i = 0; i < m_size; i++) {
+        m_arr[i] = value;
+    }
+}
+
+template <typename T>
 void Vector<T>::add(T value) {
     if (m_size == m_capacity) {
-        unsigned int tmp_capacity = m_capacity % 2 == 1 ? static_cast<unsigned int>(m_capacity * 1.5 + 1) : static_cast<unsigned int>(m_capacity * 1.5);
-        T *tmp_arr = new T[tmp_capacity];
-        for (int i = 0; i < m_capacity; i++) {
-            tmp_arr[i] = m_arr[i];
-        }
-        delete[] m_arr;
-        m_arr = tmp_arr;
-        m_capacity = tmp_capacity;
+        addCapacity();
     }
     m_arr[m_size] = value;
     m_size++;
@@ -100,10 +119,46 @@ T Vector<T>::pop() {
 }
 
 template <typename T>
-void Vector<T>::print() {
-    std::cout << "Vector start: " << std::endl;
-    for (int i = 0; i < m_size; i++) {
-        std::cout << m_arr[i] << std::endl;
+void Vector<T>::insert(int index, T value) {
+    if (0 <= index <= m_size) {
+        if (m_size == m_capacity) {
+            addCapacity();
+        }
+        T *tmp_arr = new T[m_capacity];
+        for (int i = 0; i < m_size + 1; i++) {
+            if (i == index) {
+                tmp_arr[i] = value;
+                i++;
+            }
+            tmp_arr[i] = i < index ? m_arr[i] : m_arr[i - 1];
+        }
+        delete[] m_arr;
+        m_arr = tmp_arr;
+        m_size++;
     }
-    std::cout << "Vector end. " << std::endl;
+    else throw "Index out of bounds.";
+}
+
+template <typename T>
+void Vector<T>::remove(int index) {
+    if (0 <= index < m_size) {
+        T *tmp_arr = new T[m_capacity];
+        for (int i = 0; i < m_size - 1; i++) {
+            if (i != index) {
+                tmp_arr[i] = i < index ? m_arr[i] : m_arr[i + 1];
+            }
+        }
+        delete[] m_arr;
+        m_arr = tmp_arr;
+        m_size--;
+    }
+    else throw "Index out of bounds.";
+}
+
+template <typename T>
+void Vector<T>::print() {
+    for (int i = 0; i < m_size - 1; i++) {
+        std::cout << m_arr[i] << ", ";
+    }
+    std::cout << m_arr[m_size - 1] << std::endl;
 }
