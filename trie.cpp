5f3cbd558c84bdf203da68fc784af67b40d7494f
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2021-05-27 17:03:43 -0400

    Implement search method

--- trie.cpp
+++ trie.cpp
@@ -2,6 +2,7 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <iostream>
 #include <algorithm>
 #include <unordered_map>
 
@@ -12,11 +13,18 @@ struct Node {
     std::unordered_map<char, std::shared_ptr<Node>> children;
 };
 
+std::ostream& operator << (std::ostream& o, const Node& a)
+{
+    o << "value: " << a.value << " count: " << a.count << std::endl;
+    return o;
+}
+
 class Trie {
 private:
-    std::shared_ptr<Node> m_root;
+
     std::size_t m_size;
 public:
+    std::shared_ptr<Node> m_root;
     Trie();
     void insert(std::string& str);
     std::size_t search(std::string& str);
@@ -29,10 +37,11 @@ Trie::Trie() {
 }
 
 void Trie::insert(std::string& str) {
+    m_size++;
+    m_root->count++;
     std::shared_ptr<Node> curr = m_root;
     for (char& value : str) {
-        curr->count++;
-        auto children = curr->children;
+        auto& children = curr->children;
         auto iter = children.find(value);
         if (iter != children.end()) {
             curr = iter->second;
@@ -40,12 +49,22 @@ void Trie::insert(std::string& str) {
             auto next = std::make_shared<Node>(Node{value, 0, curr, std::unordered_map<char, std::shared_ptr<Node>>()});
             children.insert({ value, next });
             curr = next;
+            curr->count++;
         }
     }
 }
 
 std::size_t Trie::search(std::string& str) {
-    return 0;
+    std::shared_ptr<Node> curr = m_root;
+    for (char& value : str) {
+        auto& children = curr->children;
+        if (children.count(value) > 0) {
+            curr = children.at(value);
+        } else {
+            return 0;
+        }
+    }
+    return curr->count;
 }
 
 std::size_t Trie::search_prefix(std::string& prefix) {
