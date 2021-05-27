6f2c30a74d476dba55678e3256f9d2b7d4a07346
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2021-05-27 16:08:08 -0400

    Implement insert method

--- trie.cpp
+++ trie.cpp
@@ -3,38 +3,51 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 struct Node {
+    char value;
     std::size_t count;
-    std::string value;
     std::shared_ptr<Node> parent;
-    std::vector<std::shared_ptr<Node>> children;
+    std::unordered_map<char, std::shared_ptr<Node>> children;
 };
 
-class trie {
+class Trie {
 private:
     std::shared_ptr<Node> m_root;
     std::size_t m_size;
 public:
-    trie();
+    Trie();
     void insert(std::string& str);
     std::size_t search(std::string& str);
     std::size_t search_prefix(std::string& prefix);
 };
 
-trie::trie() {
-    m_root = std::make_shared<Node>(Node{0, "", nullptr, std::vector<std::shared_ptr<Node>>()});
+Trie::Trie() {
+    m_root = std::make_shared<Node>(Node{'\0', 0, nullptr, std::unordered_map<char, std::shared_ptr<Node>>()});
     m_size = 0;
 }
 
-void trie::insert(std::string& str) {
-
+void Trie::insert(std::string& str) {
+    std::shared_ptr<Node> curr = m_root;
+    for (char& value : str) {
+        curr->count++;
+        auto children = curr->children;
+        auto iter = children.find(value);
+        if (iter != children.end()) {
+            curr = iter->second;
+        } else {
+            auto next = std::make_shared<Node>(Node{value, 0, curr, std::unordered_map<char, std::shared_ptr<Node>>()});
+            children.insert({ value, next });
+            curr = next;
+        }
+    }
 }
 
-std::size_t trie::search(std::string& str) {
+std::size_t Trie::search(std::string& str) {
     return 0;
 }
 
-std::size_t trie::search_prefix(std::string& prefix) {
+std::size_t Trie::search_prefix(std::string& prefix) {
     return 0;
 }
